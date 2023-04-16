54cae59a7204a0e210f755d859c8fb476a7e4df6
Anna Nawrocka <77083@student.ue.poznan.pl>
2023-04-16 18:01:16 +0200

    usunięto TODO, dodano trapez

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -10,8 +10,6 @@ obwod = a + b + c
 print("Pole trojkata wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))
 
 # romb
-# TODO
-
 a = 8
 
 h = 6
@@ -32,3 +30,15 @@ pole = pi * r ** 2
 obwod = 2 * pi * r
 
 print("Pole koła wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))
+
+# trapez
+
+a = 10
+b = 5
+c = 8
+h = 4
+
+pole = 1/2 * (a + c) * h
+obwod = a + b + b + c
+
+print("Pole trapezu wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))

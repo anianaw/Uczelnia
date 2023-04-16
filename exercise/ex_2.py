a815a024d3aa6b3d00809b0e082d392f7df0f9a8
Anna Nawrocka <77083@student.ue.poznan.pl>
2023-04-16 17:52:54 +0200

    dodaj koło

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -20,3 +20,15 @@ pole = a * h
 obwod = 4 * a
 
 print("Pole rombu wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))
+
+# koło
+import math
+
+r = 9
+pi = math.pi
+print(pi)
+
+pole = pi * r ** 2
+obwod = 2 * pi * r
+
+print("Pole koła wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))

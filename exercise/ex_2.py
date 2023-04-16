1954167b269f576b27783d135a7b4da28618cd16
Anna Nawrocka <77083@student.ue.poznan.pl>
2023-04-16 17:42:07 +0200

    dodaj romb

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -7,4 +7,16 @@ h = 6
 pole = 1/2 * a * h
 obwod = a + b + c 
 
-print("Pole trojkata wynosi " + str(pole) + "a , obwod wynosi " + str(obwod))
+print("Pole trojkata wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))
+
+# romb
+# TODO
+
+a = 8
+
+h = 6
+
+pole = a * h
+obwod = 4 * a
+
+print("Pole rombu wynosi " + str(pole) + ", a obwod wynosi " + str(obwod))

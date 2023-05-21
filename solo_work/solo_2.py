0a1a07b9e6cef65b56652e4935c42a991ed8d557
Anna Nawrocka <77083@student.ue.poznan.pl>
2023-05-21 10:14:54 +0200

    dodano trójkąt, trapez, rownoleglobok

--- solo_work/solo_2.py
+++ solo_work/solo_2.py
@@ -3,7 +3,7 @@ def trojkat(bok_a, bok_b, bok_c, h_a):
     obwod = bok_a + bok_b + bok_c
     return pole, obwod 
     
-print(f'Pole i obwod wynosza: {trojkat(10,15,12,8)}')
+print(f'Pole i obwod trójkąta wynosza: {trojkat(10,15,12,8)}')
 
 # trapez
 
@@ -12,7 +12,7 @@ def trapez(bok_a, bok_b, bok_c, h_a):
     obwod = bok_a + bok_b + 2 * bok_c
     return pole, obwod
 
-print(f'Pole i obwod wynosza: {trapez(10, 8, 6, 3)}')
+print(f'Pole i obwod trapeza wynosza: {trapez(10, 8, 6, 3)}')
 
 # rownoległobok
 
@@ -21,6 +21,6 @@ def rownoleglobok(bok_a, bok_b, h_a):
     obwod = 2 * bok_a + 2 * bok_b 
     return pole, obwod
 
-print(f'Pole i obwod wynosza: {rownoleglobok(7, 12, 5)}')
+print(f'Pole i obwod wynosza rownolegloboku: {rownoleglobok(7, 12, 5)}')
 
 
